0d3b29a1e2ac59ec600326d593c01fb65d265382
SibilJoeKurian <Sibil.kurian@gmail.com>
2019-09-30 00:47:58 -0400

    Complete Help

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -1,23 +1,28 @@
 package Driver;
 
-
 import Request.GetRequest;
 import Request.HelpRequest;
 import Request.PostRequest;
 
 public class Driver {
 	public static void main(String[] args) {
-		System.out.println(args[1]);
-		String request = args[1];
-		switch (request) {
-		case "get":
-			GetRequest get = new GetRequest(args);
-			break;
-		case "post":
-			PostRequest post=new PostRequest(args);
-			break;
-		case "help":
-			HelpRequest help=new HelpRequest(args);
-		}
+		if (args.length > 1)
+			switch (args[1]) {
+			case "get":
+				GetRequest get = new GetRequest(args);
+				break;
+			case "post":
+				PostRequest post = new PostRequest(args);
+				break;
+			case "help":
+				HelpRequest help = new HelpRequest(args);
+				help.helpDriver();
+				break;
+			default:
+				System.out.println(HelpRequest.INVALID);
+				break;
+			}
+		else
+			System.out.println(HelpRequest.INVALID);
 	}
 }
--- src/Request/HelpRequest.java
+++ src/Request/HelpRequest.java
@@ -1,15 +1,40 @@
 package Request;
 
 public class HelpRequest {
-	String[] arguments;
-
+	static String[] arguments;
+	public static final String INVALID = "INVALID COMMAND LINE ARGUMENTS\n\n"
+			+ "For information about usage type \"httpc help\"";
+			
 	public HelpRequest(String[] args) {
 		arguments = args;
-
 	}
 
-	public void displayArguments() {
-		for (int i = 0; i < arguments.length; i++)
-			System.out.println(arguments[i]);
+	public static void helpDriver() {
+		if (arguments.length == 3)
+			if (arguments[2].equals("post"))
+				System.out.println(
+						"httpc help post\n" + "usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n"
+								+ "Post executes a HTTP POST request for a given URL with inline data or from file.\n"
+								+ "-v\tPrints the detail of the response such as protocol, status, and headers.\n"
+								+ "-h key:value\tAssociates headers to HTTP Request with the format 'key:value'.\n"
+								+ "-d string\tAssociates an inline data to the body HTTP POST request.\n"
+								+ "-f file\tAssociates the content of a file to the body HTTP POST request.\n\n"
+								+ "Either [-d] or [-f] can be used but not both.");
+			else if (arguments[2].equals("get"))
+				System.out.println("httpc help get\n" + "usage: httpc get [-v] [-h key:value] URL\n"
+						+ "Get executes a HTTP GET request for a given URL.\n"
+						+ "-v\tPrints the detail of the response such as protocol, status, and headers.\n"
+						+ "-h key:value\tAssociates headers to HTTP Request with the format 'key:value'.");
+			else
+				System.out.println("INVALID COMMAND LINE ARGUMENTS\n\n");
+		else if (arguments.length == 1 && arguments[1].equals("help"))
+			System.out.println("httpc is a curl-like application but supports HTTP protocol only.\n"
+					+ "Usage:\nhttpc command [arguments]\n" + "The commands are:\n"
+					+ "get\texecutes a HTTP GET request and prints the response.\n"
+					+ "post\texecutes a HTTP POST request and prints the response.\n" + "help\tprints this screen.\n\n"
+					+ "Use \"httpc help [command]\" for more information about a command.");
+		else
+			System.out.println("INVALID COMMAND LINE ARGUMENTS\n\n");
 	}
+
 }
